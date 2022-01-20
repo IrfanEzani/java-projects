a41d2e9073e3a74d643e8d79112b6222b1096c20
Irfan Ezani <irfanezani@gmail.com>
2022-01-20 12:14:53 -0500

    rent calculator update

--- rentCalculator.java
+++ rentCalculator.java
@@ -1,28 +1,56 @@
-import java.util.Scanner;
 /**
  * 
  * rentCalculator: calculate house rent
  */
 public class rentCalculator {
     public static void main(String[] args) {
-        int rentPrice = 3725;
-        int roomA, roomB, roomC, sharedRoom;
-        
-        Scanner s = new Scanner(System.in);
-        System.out.println("set room A price:");
-        roomA = s.nextInt();
-        System.out.println("set room B price:");
-        roomB = s.nextInt();
-        System.out.println("set room C price:");
-        roomC = s.nextInt();
+        double rentPrice = 4750;
+        double utilities= 50;
+        double houseTotal = rentPrice + (utilities*6);
+        double roomA, roomB, roomC, roomD;
+        double aRatio = 4.25, bRatio = 4, cRatio = 3.5, dRatio = 3.25;
+        double totalRatio = (aRatio*2) + (bRatio*3) + cRatio + dRatio;
 
-        sharedRoom = (rentPrice - roomA - (roomB * 2) - roomC) / 2;
+        roomA = (houseTotal / totalRatio) * aRatio; 
+        roomB = (houseTotal / totalRatio) * bRatio; 
+        roomC = (houseTotal / totalRatio) * cRatio; 
+        roomD = (houseTotal / totalRatio) * dRatio; 
+        
+        System.out.println("roomA = " + (int) roomA);
+        System.out.println("roomB = " + (int) roomB);
+        System.out.println("roomC = " + (int) roomC);
+        System.out.println("roomD = " + (int) roomD);
+        roomD /= 6;
+        System.out.println("roomA with utilities and D = " + (int)(roomA+roomD));
+        System.out.println("roomB with utilities and D = " + (int)(roomB+roomD));
+        System.out.println("roomC with utilities and D = " + (int)(roomC+roomD));
+        System.out.println("houseTotal = " + houseTotal);
+
+        System.out.println("left:" + (int)((houseTotal - 
+        ((roomA * 2)) -
+        ((roomB * 3)) - 
+        (roomC) - (roomD * 6))
+        ));
+        
+        /* Scanner s = new Scanner(System.in);
+        System.out.println("set room A price:");
+        roomA = s.nextDouble();
+        System.out.println("set room B price:");
+        roomB = s.nextDouble();
+        System.out.println("set room C price:");
+        roomC = s.nextDouble();
+        /* System.out.println("set room D price:");
+        roomD = s.nextDouble(); 
+
+        roomA *= 2;
+        roomB *= 3;
         System.out.println("total price for all=");
-        System.out.println("room A=" + roomA);
-        System.out.println("room B=" + roomB);
-        System.out.println("room C=" + roomC);
-        System.out.println("shared room=" + sharedRoom);
-        s.close();
+        System.out.println("room A total = " + roomA);
+        System.out.println("room B total = " + roomB);
+        System.out.println("room C total = " + roomC);
+        double total = roomA+roomB+roomC;
+        System.out.println("rent price left=" + (fullTotal-total));
+        s.close(); */
     }
     
 }
